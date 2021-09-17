462188cbdf3f98efed2e8bf439e714d131307237
Sepanta <Sepanta_97@Yahoo.com>
2021-09-17 21:18:54 +0430

    more header styles

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Fragment } from 'react'
-import classes from'./Header.module.css'
-import MealsPic from '../../assets/meals.jpeg'
+import React from "react";
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import MealsPic from "../../assets/meals.jpeg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
-    return (
-        <Fragment>
-            <header className={classes.header}>
-                <h1>Food Order With Context</h1>
-                <button>Cart</button>
-            </header>
-            <div className={classes["main-image"]}>
-                <img src={MealsPic} alt="meals" />
-            </div>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Food Order With Context</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={MealsPic} alt="meals" />
+      </div>
+    </Fragment>
+  );
+};
 
-export default Header
+export default Header;
