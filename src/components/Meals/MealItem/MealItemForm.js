f8dd16916fda6a7aa6f4c0a757f45e06554e96f8
Sepanta <Sepanta_97@Yahoo.com>
2021-09-20 19:39:59 +0430

    configure input component inside the mealform component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import classes from './MealItemForm.module.css'
+import React from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
 const MealItemForm = () => {
-    return (
-        <form className={classes.form}>
-            <input type="text" />
-            <button>+ Add</button>
-            
-        </form>
-    )
-}
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "10",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
