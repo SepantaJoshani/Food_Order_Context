a31c4a98cdf1e74f9eac365a087ed6876339845a
Sepanta <Sepanta_97@Yahoo.com>
2021-09-21 23:02:09 +0430

    adding item with useReducer in context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,39 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const initialState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items:updatedItems,
+      totalAmount:updatedTotalAmount
+    }
+    default:
+      return initialState;
+  }
+};
+
 const CartProvider = (props) => {
-  const addItemHandler = () => {};
-  const removeItemHandler = () => {};
+  const [cartState, dispatch] = useReducer(cartReducer, initialState);
+
+  const addItemHandler = (item) => {
+    dispatch({ type: "ADD", item: item });
+  };
+  const removeItemHandler = (id) => {
+    dispatch({ type: "REMOVE", id: id });
+  };
 
   const cartcontext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   };
@@ -18,4 +45,4 @@ const CartProvider = (props) => {
   );
 };
 
-export default CartProvider
+export default CartProvider;
