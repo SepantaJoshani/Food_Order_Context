4ef3827193b6a5563bfb18346fd302ce3be50179
Sepanta <Sepanta_97@Yahoo.com>
2021-09-23 21:13:54 +0330

    adding animation to cart btn

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,22 +1,39 @@
-import React, { useContext } from 'react'
-import classes from './HeaderCartButton.module.css'
-import CartIcon from '../Cart/CartIcon'
-import CartContext from '../../store/cart-context'
+import React, { useContext, useEffect, useState } from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-    const cartCtx = useContext(CartContext)
-    const cartItemsNum = cartCtx.items.reduce((curNumber,item)=>{
-        return curNumber + item.amount
-    },0)
-    return (
-        <button className={classes.button} onClick={props.show}>
-            <span className={classes.icon}>
-                <CartIcon />
-            </span>
-            <span className={classes.Cart__txt}>Your Cart</span>
-            <span className={classes.badge}>{cartItemsNum}</span>
-        </button>
-    )
-}
+  const [animate, setAnimate] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const cartItemsNum = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-export default HeaderCartButton
+  const animateClass = `${classes.button} ${animate ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setAnimate(false);
+    }, 300);
+    setAnimate(true);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+  return (
+    <button className={animateClass} onClick={props.show}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span className={classes.Cart__txt}>Your Cart</span>
+      <span className={classes.badge}>{cartItemsNum}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
